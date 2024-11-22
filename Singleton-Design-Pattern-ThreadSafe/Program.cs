1911fc1e21fbf0748e558a98d3424362991a75db
Priti-Pawar <pritipawar161299@gmail.com>
2024-11-22 23:13:40 +0530

    Adding Parellelism Methods to check thread safe

--- Singleton-Design-Pattern-ThreadSafe/Program.cs
+++ Singleton-Design-Pattern-ThreadSafe/Program.cs
@@ -6,9 +6,37 @@ namespace Singleton_Design_Pattern_ThreadSafe
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Without Parellelism:-");
             Console.WriteLine(UniqueKeyGenerator.GetInstance().GetUniqueKey);
             Console.WriteLine(UniqueKeyGenerator.GetInstance().GetUniqueKey);
             Console.WriteLine(UniqueKeyGenerator.GetInstance().GetUniqueKey);
+
+            // Invoking multiple methods parellely
+
+            Console.WriteLine("\nWith Parellelism:-");
+            Parallel.Invoke(
+                    ()=> PrintUniqueKeyThreadFirst(),
+                    ()=> PrintUniqueKeyThreadSecond(),
+                    ()=> PrintUniqueKeyThreadThird()
+                );
+
+            Console.WriteLine("\nPress Any Key...");
+            Console.ReadLine();
+        }
+
+        private static void PrintUniqueKeyThreadFirst()
+        {
+            Console.WriteLine("1st:" + UniqueKeyGenerator.GetInstance().GetUniqueKey);
+        }
+
+        private static void PrintUniqueKeyThreadSecond()
+        {
+            Console.WriteLine("2nd:" + UniqueKeyGenerator.GetInstance().GetUniqueKey);
+        }
+
+        private static void PrintUniqueKeyThreadThird()
+        {
+            Console.WriteLine("3rd:" + UniqueKeyGenerator.GetInstance().GetUniqueKey);
         }
     }
 }
