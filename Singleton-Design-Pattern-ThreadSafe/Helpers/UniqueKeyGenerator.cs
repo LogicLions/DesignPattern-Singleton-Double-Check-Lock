2d96c8de745719428f45d56006d708da81999f40
Priti-Pawar <pritipawar161299@gmail.com>
2024-11-22 22:54:03 +0530

    Adding singleton design class - UniqueKeyGenerator

---
+++ Singleton-Design-Pattern-ThreadSafe/Helpers/UniqueKeyGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Design_Pattern_ThreadSafe.Helpers
+{
+    public sealed class UniqueKeyGenerator
+    {
+        private static UniqueKeyGenerator instance = null!;
+        private static object locking = new object();
+
+        private Guid UniqueKey;
+
+        private UniqueKeyGenerator(Guid uniqueKey) 
+        {
+            this.UniqueKey = uniqueKey;
+        }
+
+        public static UniqueKeyGenerator GetInstance()
+        {
+            if (instance == null)
+            {
+                lock (locking) 
+                {
+                    if (instance == null)
+                    {
+                        instance = new UniqueKeyGenerator(Guid.NewGuid());
+                    }
+                }
+            }
+            return instance;
+        }
+
+        public Guid GetUniqueKey { get => this.UniqueKey; }
+    }
+}
--- Singleton-Design-Pattern-ThreadSafe/Program.cs
+++ Singleton-Design-Pattern-ThreadSafe/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace Singleton_Design_Pattern_ThreadSafe
+﻿using Singleton_Design_Pattern_ThreadSafe.Helpers;
+
+namespace Singleton_Design_Pattern_ThreadSafe
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(UniqueKeyGenerator.GetInstance().GetUniqueKey);
+            Console.WriteLine(UniqueKeyGenerator.GetInstance().GetUniqueKey);
         }
     }
 }
